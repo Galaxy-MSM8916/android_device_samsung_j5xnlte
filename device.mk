a36762b02dcef66bd8cfb59905a609c06bedc38c
Vincent <vincent.zvikaramba@mail.utoronto.ca>
2017-11-18 22:27:01 -0500

    j5nlte: Disable nfc
    Change-Id: I02e1e85861adfa69caa6bfd7be1deaa0911e0d57

--- device.mk
+++ device.mk
@@ -13,24 +13,24 @@ LOCAL_PATH := device/samsung/j5nlte
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 #NFC
-PRODUCT_PACKAGES += \
-	android.hardware.nfc@1.0-impl \
-	android.hardware.nfc@1.0-service \
-	com.android.nfc_extras \
-	libnfc_nci \
-	libnfc_nci_jni \
-	nfc_nci.pn54x.msm8916 \
-	NfcNci \
-	Tag
+#PRODUCT_PACKAGES += \
+#	android.hardware.nfc@1.0-impl \
+#	android.hardware.nfc@1.0-service \
+#	com.android.nfc_extras \
+#	libnfc_nci \
+#	libnfc_nci_jni \
+#	nfc_nci.pn54x.msm8916 \
+#	NfcNci \
+#	Tag
 
 # NFC configs
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
-	$(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
+#	$(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+#	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
 
 # Permissions
-PRODUCT_COPY_FILES += \
-	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+#PRODUCT_COPY_FILES += \
+#	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+#	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+#	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
