446c824e2cbfc08efd8be67ee6800c1f0db9177d
Blue Dogerino <fsalamic16@gmail.com>
2018-03-15 01:18:32 +0100

    j5xnlte: fixed omni and rr mk files
    Change-Id: I3212bcf972d06c7a6fcfe78bc738b2505b577b74

--- omni_j5xnlte.mk
+++ omni_j5xnlte.mk
@@ -1,12 +1,12 @@
 
 # Inherit from common
-$(call inherit-product, device/samsung/j5x-common/rr.mk)
+$(call inherit-product, device/samsung/j5x-common/omni.mk)
 
-$(call inherit-product, device/samsung/j5xlte/device.mk)
+$(call inherit-product, device/samsung/j5xnlte/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := j5xlte
-PRODUCT_NAME := rr_j5xlte
+PRODUCT_DEVICE := j5xnlte
+PRODUCT_NAME := omni_j5xnlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J510FN
 PRODUCT_MANUFACTURER := samsung
