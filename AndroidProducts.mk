05ab311cc7a30dd36ee31d1e7ecda415ed4ebab9
Dan Willemsen <dwillemsen@google.com>
2020-06-18 03:31:07 +0800

    j5xnlte: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Ib1a7008013518e912cf6b381e5a6297a760e0c1f

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,3 +3,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/omni_j5xnlte.mk \
 	$(LOCAL_DIR)/rr_j5xnlte.mk \
 	$(LOCAL_DIR)/lineage_j5xnlte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_j5xnlte-eng \
+    lineage_j5xnlte-userdebug \
+	lineage_j5xnlte-user
